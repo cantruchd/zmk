f6ccb5932f94228dd385d6c3a0749b4e9c9f81e0
cantruchd <truc294@gmail.com>
2025-10-27 14:46:30 +0800

    Update types.h

--- app/include/zmk/split/transport/types.h
+++ app/include/zmk/split/transport/types.h
@@ -30,23 +30,20 @@ enum zmk_split_transport_peripheral_event_type {
     ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_SENSOR_EVENT,
     ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_INPUT_EVENT,
     ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_BATTERY_EVENT,
+    ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_WPM_EVENT,  // ← THÊM DÒNG NÀY
 };
 
 struct zmk_split_transport_peripheral_event {
     enum zmk_split_transport_peripheral_event_type type;
-
     union {
         struct {
             uint8_t position;
             uint8_t pressed;
         } key_position_event;
-
         struct {
             struct zmk_sensor_channel_data channel_data;
-
             uint8_t sensor_index;
         } sensor_event;
-
         struct {
             uint8_t reg;
             uint8_t sync;
@@ -54,10 +51,12 @@ struct zmk_split_transport_peripheral_event {
             uint16_t code;
             int32_t value;
         } input_event;
-
         struct {
             uint8_t level;
         } battery_event;
+        struct {                   // ← THÊM STRUCT MỚI
+            uint8_t wpm;
+        } wpm_event;
     } data;
 } __packed;
 
@@ -66,11 +65,11 @@ enum zmk_split_transport_central_command_type {
     ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_INVOKE_BEHAVIOR,
     ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SET_PHYSICAL_LAYOUT,
     ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SET_HID_INDICATORS,
+    ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SEND_WPM,  // ← THÊM DÒNG NÀY
 } __packed;
 
 struct zmk_split_transport_central_command {
     enum zmk_split_transport_central_command_type type;
-
     union {
         struct {
             char behavior_dev[16];
@@ -79,13 +78,14 @@ struct zmk_split_transport_central_command {
             uint8_t event_source;
             uint8_t state;
         } invoke_behavior;
-
         struct {
             uint8_t layout_idx;
         } set_physical_layout;
-
         struct {
             zmk_hid_indicators_t indicators;
         } set_hid_indicators;
+        struct {                   // ← THÊM STRUCT MỚI
+            uint8_t wpm;
+        } send_wpm;
     } data;
 } __packed;
