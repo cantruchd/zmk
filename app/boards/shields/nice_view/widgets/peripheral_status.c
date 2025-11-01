2c3be784c0e651fe25cc62cb619a99f14daa3d56
cantruchd <truc294@gmail.com>
2025-11-01 20:04:27 +0800

    Update peripheral_status.c

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -301,12 +301,12 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     // Top canvas
     lv_obj_t *top = lv_canvas_create(widget->obj);
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
-    lv_canvas_set_buffer(top, widget->cbuf2, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     // WPM canvas
     lv_obj_t *wpm_canvas = lv_canvas_create(widget->obj);
     lv_obj_align(wpm_canvas, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_canvas_set_buffer(wpm_canvas, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(wpm_canvas, widget->cbuf2, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
     
 
     widget->state.battery = 0;
@@ -329,8 +329,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     widget_wpm_status_init();
 
     
-    draw_top(widget->obj, widget->cbuf2, &widget->state);   
-    draw_wpm(widget->obj, widget->cbuf, &widget->state);
+    draw_top(widget->obj, widget->cbuf, &widget->state);   
+    draw_wpm(widget->obj, widget->cbuf2, &widget->state);
     
     LOG_INF("Peripheral WPM widget initialized with border, grid, max/avg display");
     return 0;
