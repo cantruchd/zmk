68128470da8f7d848109dbbc5b9f22d719c2a21a
cantruchd <truc294@gmail.com>
2025-11-07 22:27:04 +0800

    Update status.c

--- app/boards/shields/nice_view/widgets/status.c
+++ app/boards/shields/nice_view/widgets/status.c
@@ -348,7 +348,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
     lv_obj_t *middle = lv_canvas_create(widget->obj);
     lv_obj_align(middle, LV_ALIGN_TOP_LEFT, 8, 0);//24-16
-    lv_canvas_set_buffer(middle, widget->cbuf2, 84, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(middle, widget->cbuf2, 84, 84, LV_IMG_CF_TRUE_COLOR);
     lv_obj_t *bottom = lv_canvas_create(widget->obj);
     lv_obj_align(bottom, LV_ALIGN_TOP_LEFT, -60, 0); //-44-16
     lv_canvas_set_buffer(bottom, widget->cbuf3, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
