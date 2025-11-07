7e2dfbaab289afafe150f920ca1caf5a99ac8cc0
cantruchd <truc294@gmail.com>
2025-11-07 21:43:48 +0800

    Update peripheral_status.h

--- app/boards/shields/nice_view/widgets/peripheral_status.h
+++ app/boards/shields/nice_view/widgets/peripheral_status.h
@@ -14,7 +14,7 @@
 struct zmk_widget_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf[CANVAS_SIZE * 160];
     struct status_state state;
 };
 
