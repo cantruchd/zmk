820dc69053c7d99c18e5a363b27245687a62d29b
cantruchd <truc294@gmail.com>
2025-11-08 08:19:07 +0800

    Update status.c

--- app/boards/shields/nice_view/widgets/status.c
+++ app/boards/shields/nice_view/widgets/status.c
@@ -88,10 +88,9 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
 
     lv_canvas_draw_text(canvas, 0, 0, CANVAS_SIZE, &label_dsc, output_text);
 
-    // Draw WPM
-    // Draw WPM box - no gap below
-    lv_canvas_draw_rect(canvas, 0, 21, 68, 47, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 1, 22, 66, 45, &rect_black_dsc);
+    // Draw WPM    
+    lv_canvas_draw_rect(canvas, 0, 21, 68, 42, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 1, 22, 66, 40, &rect_black_dsc);
 
     // Find max WPM
     int max = 0;
